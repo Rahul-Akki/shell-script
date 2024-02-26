6d6e1cafb92fcf5485c96eb35201ca160fc82cda
Rahul-Akki <rahul.akireddy@gmail.com>
2024-02-26 18:07:00 -0500

    shell script

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,5 +1,5 @@
-#! /bin/bash/
+#! /bin/bash/ # this is called shebang --> this is to initate the shell script
 
 # this is commant
 
-echo "Hello Shell Script" #echo --> print
+echo "Hello Shell Script" # echo --> print
