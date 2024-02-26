3b90e46ccc2c7da38e00f1d7478023f3ff8a85a1
Rahul-Akki <rahul.akireddy@gmail.com>
2024-02-26 18:46:05 -0500

    shell script

--- 02-varibles.sh
+++ 02-varibles.sh
@@ -3,7 +3,7 @@
 person1=ramesh
 person2=Suresh
 
-echo "$person1 :: Hello person2, Good morning"
-echo "person2 :: Hi $person1, Very Good Morning"
-echo "$person1 :: How are you person2 ?"
-echo "person2 :: I am good $person1, How are you ?"
+echo "$person1 :: Hello $person2, Good morning"
+echo "$person2 :: Hi $person1, Very Good Morning"
+echo "$person1 :: How are you $person2 ?"
+echo "$person2 :: I am good $person1, How are you ?"
