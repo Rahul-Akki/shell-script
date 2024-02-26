2430e6363b73862d35c99be4dbfd2bc81143735d
Rahul-Akki <rahul.akireddy@gmail.com>
2024-02-26 18:38:37 -0500

    shell script

---
+++ 02-varibles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+echo "Ramesh :: Hello Suresh, Good morning"
+echo "suredh :: Hi Ramesh, Very Good Morning"
+echo "Ramesh :: How are you suresh ?"
+echo "Suresh :: I am good Ramesh, How are you ?"
