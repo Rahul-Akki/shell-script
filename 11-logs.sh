aea61b6dc4b3c72fafb14781935bb9c26bef95d1
Rahul-Akki <rahul.akireddy@gmail.com>
2024-02-27 17:00:41 -0500

    shell script

---
+++ 11-logs.sh
@@ -0,0 +1,33 @@
+#!bin/bash
+
+VALIDATE(){ #3 --> Validation function and call it whenever required.
+    if [ $1 -ne 0 ]
+    then
+        echo "ERROR : $2 ...failed"
+        exit 1
+    else
+        echo "STATUS : $2 ...SUCCESS" 
+    fi
+}
+
+TIMESTAMP=$(date)
+LOGFILE="/TEMP/$0-$TIMESTAMP.log"
+
+
+
+ID=$(id -u) # 1 --> Sudo access validation
+if [ $ID -ne 0 ]
+then
+    echo "ERROR : Please run this script with root access"
+    exit 1 
+else
+    echo "Your are root user"
+fi 
+
+yum install mysql -y & >> $LOGFILE # 2 --> MySQL installation
+
+VALIDATE $? "MySQL Installation"
+
+yum install git -y & >> $LOGFILE
+
+VALIDATE $? "GIT Installation"
