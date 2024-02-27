0e1de67cc40b489a90032e334a031d8d3e39dd5d
Rahul-Akki <rahul.akireddy@gmail.com>
2024-02-26 19:00:50 -0500

    shell script

---
+++ 04-varibles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+person1=$1
+person2=$2
+
+echo "$person1 :: Hello $person2, Good morning"
+echo "$person2 :: Hi $person1, Very Good Morning"
+echo "$person1 :: How are you $person2 ?"
+echo "$person2 :: I am good $person1, How are you ?"
+
+# Commands to use 
+# sh 04-varibles.sh <$1> <$2>
+# sh 04-varibles.sh ramesh suresh
