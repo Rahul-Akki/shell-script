8c1afe2fc2b7168d0e9411a62308081250fc4c25
Rahul-Akki <rahul.akireddy@gmail.com>
2024-02-26 19:35:39 -0500

    shell script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -6,4 +6,4 @@ echo "First value: ${FRUITS[0]}"  # Index will be startion from 0 in Array
 echo "Second value: ${FRUITS[1]}" 
 echo "Third value: ${FRUITS[2]}"
 
-echo "All value's: ${FRUITS[@]}" 
+echo "All value's: ${FRUITS[@]}" # @ --> is used to choose all the valus of the array.
