001aa8c5012eceebb30cd9168dc13ecd76cfdc93
Rahul-Akki <rahul.akireddy@gmail.com>
2024-02-27 13:58:26 -0500

    shell script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,7 +2,7 @@
 
 FRUITS=("Apple" "Banana" "Mango") # Array --> Attaching multiple values to the singlr variple
 
-echo "First value: ${FRUITS[0]}"  # Index will be startion from 0 in Array
+echo "First value: ${FRUITS[0]}"  # Index will be from 0 in Array
 echo "Second value: ${FRUITS[1]}" 
 echo "Third value: ${FRUITS[2]}"
 
