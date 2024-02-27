bb7c22182d3dff204b58212108ac2e211ccc16a8
Rahul-Akki <rahul.akireddy@gmail.com>
2024-02-27 14:31:34 -0500

    shell script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -1,11 +1,28 @@
 #!bin/bash
 
+# 1 --> Sudo access validation
 ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then
-    echo "ERROR : Please run the script with root access"
+    echo "ERROR : Please run this script with root access" #stop --> clear the error --> then proceed to next step
+    exit 1 # we can can give any number other than 0. -->   # EXIT STATUS --> echo $? = 0
+
+             #shell scrip will not stop if we get an error. Its our responsibility to check and proceed the script. 
+           
 else
     echo "Your are root user"
-fi
+fi          # fi is used to end the if condition
 
+            
+# 2 --> MySQL installation
+
+yum install myslql -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR : Installing MySQL is failed"
+    exit 1
+else
+    echo "ERROR : Installing MySQL is SUCCESS"
+fi
