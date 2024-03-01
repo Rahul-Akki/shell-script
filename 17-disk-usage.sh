15607c0decfd5cad0580591aa7341a776fecd59b
Rahul-Akki <rahul.akireddy@gmail.com>
2024-02-29 21:14:04 -0500

    shell-script

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -12,7 +12,7 @@ do
     then
         message+="High Disk Usage On $partition: $usage\n"
     fi
-done<<< $DISK_USAGE
+done <<< $DISK_USAGE
 
 echo -e " Message: $message "
 echo "$message Date $(date)" | mail -s "High Disk Usage" rahula.24a@gmail.com
