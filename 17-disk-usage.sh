3dd7e2ccbf1aaa00f4f6ea167cff419868b42d19
Rahul-Akki <rahul.akireddy@gmail.com>
2024-02-29 22:27:00 -0500

    shell-script

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -17,4 +17,4 @@ done <<< $DISK_USAGE
 echo -e " Message: $message "
 #echo "$message Date $(date)" | mail -s "High Disk Usage" rahula.24a@gmail.com
 
-sh mail.sh "Devops Team" "High Disk Alert" "message" "rahula.24a@gmail.com" "Alert High Usage"
+sh mail.sh "Devops Team" "High Disk Alert" " $message " "rahula.24a@gmail.com" "Alert High Usage"
